2a201843574984ad71e66d182d54fadaffa10fa4
Rashid Ekbal <rashidekbal.edugaon@gmail.com>
2024-07-09 13:51:49 +0530

    Drawer created

--- lib/views/landing_screens/home_screen.dart
+++ lib/views/landing_screens/home_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:home_service/colors.dart';
+import 'package:home_service/views/utils/app_size.dart';
+import 'package:home_service/views/utils/custom_widgets.dart';
+import 'package:home_service/views/utils/styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    var style = Styles();
+    var size = AppSize(context: context);
+    var customWidget = CustomWidgets();
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.notifications, color: primaryColor,))],
+      ),
+
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+              ),
+              currentAccountPictureSize: const Size.square(50),
+                currentAccountPicture: CircleAvatar(backgroundColor: primaryColor, child: Text('J', style: style.boldTextStyle(color: subPrimaryColor),),),
+                accountName: Text('John Smith', style: style.normalTextStyle(weight: boldFont, color: primaryColor),),
+                accountEmail: const Text('johnsmith@gmail.com', style: TextStyle(color: fikColor),)
+            ),
+            customWidget.profileMenu(),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/views/utils/custom_widgets.dart
+++ lib/views/utils/custom_widgets.dart
@@ -49,4 +49,26 @@ class CustomWidgets{
       )
     ],
   );
+
+  Widget profileMenu({IconData? trailing}) => Column(
+
+    children: [
+      myListTile(icon: Icons.person, title: 'My Profile',trailing: trailing, onTap: (){}),
+      myListTile(icon: Icons.favorite, title: 'My Favorites', onTap: (){}),
+      myListTile(icon: Icons.notifications, title: 'Notification', onTap: (){}),
+      myListTile(icon: Icons.calendar_month, title: 'My Bookings', onTap: (){}),
+      myListTile(icon: Icons.currency_exchange, title: 'Refer and earn', onTap: (){}),
+      myListTile(icon: Icons.email, title: 'Contact Us', onTap: (){}),
+      myListTile(icon: Icons.question_mark, title: 'Help Center', onTap: (){}),
+      myListTile(icon: Icons.logout, title: 'Logout', onTap: (){}),
+    ],
+  );
+
+  Widget myListTile({required IconData icon, required String title, IconData? trailing, VoidCallback? onTap}) => ListTile(
+    leading: Icon(icon, color: primaryColor,),
+    title: Text(title, style: Styles().normalTextStyle(color: primaryColor),),
+    trailing: Icon(trailing),
+    onTap: onTap,
+    horizontalTitleGap: 1.0,
+  );
 }
